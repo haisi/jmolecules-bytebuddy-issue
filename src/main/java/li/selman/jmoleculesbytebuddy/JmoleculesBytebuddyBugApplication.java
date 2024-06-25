3a6609302bb5d92d44eb474427dba66d7e508928
Hasan Selman Kara <hasan.selman.kara@gmail.com>
2024-06-25 19:51:50 +0200

    Init

---
+++ src/main/java/li/selman/jmoleculesbytebuddy/JmoleculesBytebuddyBugApplication.java
@@ -0,0 +1,13 @@
+package li.selman.jmoleculesbytebuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmoleculesBytebuddyBugApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JmoleculesBytebuddyBugApplication.class, args);
+    }
+
+}
---
+++ src/test/java/li/selman/jmoleculesbytebuddy/JmoleculesBytebuddyBugApplicationTests.java
@@ -0,0 +1,13 @@
+package li.selman.jmoleculesbytebuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmoleculesBytebuddyBugApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
